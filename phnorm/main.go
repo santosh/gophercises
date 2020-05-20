834a7aa67ea46b5b130cfd36669e9e64f5b6e589
Santosh Kumar <sntshkmr60@gmail.com>
2020-05-21 03:56:06 +0530

    Create DB with github.com/lib/pq

--- phnorm/main.go
+++ phnorm/main.go
@@ -1,6 +1,72 @@
 package main
 
-import "regexp"
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"regexp"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+}
+
+const (
+	host     = "localhost"
+	port     = 5432
+	username = "postgres"
+	dbname   = "gophercises_phone"
+)
+
+func main() {
+	password := os.Getenv("PASSWORD")
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", host, port, username, password)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	err = resetDB(db, dbname)
+	if err != nil {
+		panic(err)
+	}
+	db.Close()
+
+	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
+	db, err = sql.Open("postgres", psqlInfo)
+
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func resetDB(db *sql.DB, name string) error {
+	_, err := db.Exec("DROP DATABASE IF EXISTS " + name)
+	if err != nil {
+		return err
+	}
+
+	return createDB(db, name)
+}
+
+func createDB(db *sql.DB, name string) error {
+	_, err := db.Exec("CREATE DATABASE " + name)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func normalize(phone string) string {
 	re := regexp.MustCompile("\\D")
