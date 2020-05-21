48cd03764fd7bfd9e55a42fc614f04ebdaf9c5a7
Santosh Kumar <sntshkmr60@gmail.com>
2020-05-21 15:11:12 +0530

    Insert row into phone_numbers & return ID

--- phnorm/main.go
+++ phnorm/main.go
@@ -33,11 +33,11 @@ func main() {
 		panic(err)
 	}
 
-	err = resetDB(db, dbname)
-	if err != nil {
-		panic(err)
-	}
-	db.Close()
+	// err = resetDB(db, dbname)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// db.Close()
 
 	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
 	db, err = sql.Open("postgres", psqlInfo)
@@ -50,6 +50,18 @@ func main() {
 	}
 
 	createPhoneNumbersTable(db)
+	id, err := insertPhone(db, "123456789")
+	fmt.Println("id=", id)
+}
+
+func insertPhone(db *sql.DB, phone string) (int, error) {
+	statement := `INSERT INTO phone_numbers(value) VALUES($1) RETURNING id`
+	var id int
+	err := db.QueryRow(statement, phone).Scan(&id)
+	if err != nil {
+		return -1, err
+	}
+	return id, nil
 }
 
 func createPhoneNumbersTable(db *sql.DB) error {
