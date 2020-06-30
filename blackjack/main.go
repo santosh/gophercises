756086fce2d5889b8de7d0ee228a2d0f9771a6f8
Santosh Kumar <sntshkmr60@gmail.com>
2020-06-30 19:11:41 +0530

    Implements initial logic
    Hit/Stand at will.
    Dealer's hand will be hidden until stand.

--- blackjack/main.go
+++ blackjack/main.go
@@ -19,18 +19,46 @@ func (h Hand) String() string {
 	return strings.Join(strs, ", ")
 }
 
+func (h Hand) DealerString() string {
+	return h[0].String() + ", **HIDDEN**"
+}
+
+// draw fetches a card from deck of cards and returns fetched
+// card and the remaining deck
+func draw(cards []deck.Card) (deck.Card, []deck.Card) {
+	return cards[0], cards[1:]
+}
+
 func main() {
 	// create 3 deck of cards and shuffle it
 	cards := deck.New(deck.Deck(3), deck.Shuffle)
-
 	var card deck.Card
-
-	// Take first 10 cards from the shuffled deck
-	for i := 0; i < 10; i++ {
-		card, cards = cards[0], cards[1:]
-		fmt.Println(card)
+	var player, dealer Hand
+	for i := 0; i < 2; i++ {
+		for _, hand := range []*Hand{&player, &dealer} {
+			// take the first card from cards
+			card, cards = draw(cards)
+			// append it to one of the hand (either player or dealer)
+			*hand = append(*hand, card)
+			// by the end of the loop,
+			// both player and dealer will have one-one card
+		}
+		// In outer loop, we just repeat the appending of cards n number of times.
 	}
 
-	var h Hand = cards[0:3]
-	fmt.Println(h)
+	var input string
+	for input != "s" {
+		fmt.Println("Player:", player)
+		fmt.Println("Dealer:", dealer.DealerString())
+		fmt.Println("What will you do? (h)it, (s)tand")
+		fmt.Scanf("%s\n", &input)
+		switch input {
+		case "h":
+			card, cards = draw(cards)
+			player = append(player, card)
+		}
+	}
+	fmt.Println("==FINAL HANDS==")
+	fmt.Println("Player:", player)
+	fmt.Println("Dealer:", dealer)
 }
