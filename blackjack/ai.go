8a1b11b92be94b305db62f6a9ab9d8ffdecc2286
Santosh Kumar <sntshkmr60@gmail.com>
2020-07-07 12:46:45 +0530

    Add AI interface

---
+++ blackjack/ai.go
@@ -0,0 +1,43 @@
+package blackjack
+
+import (
+	"fmt"
+
+	"github.com/santosh/gophercises/deck"
+)
+
+type AI interface {
+	Bet() int
+	Play(hand []deck.Card, dealer deck.Card)
+	Summary(hand [][]deck.Card, dealer []deck.Card)
+}
+
+type HumanAI struct{}
+
+func (ai *HumanAI) Bet() int {
+	return 1
+}
+
+func (ai *HumanAI) Play(hand []deck.Card, dealer deck.Card) Move {
+	for {
+		fmt.Println("Player:", hand)
+		fmt.Println("Dealer:", dealer)
+		fmt.Println("What will you do? (h)it, (s)tand")
+		var input string
+		fmt.Scanf("%s\n", &input)
+		switch input {
+		case "h":
+			return Hit
+		case "s":
+			return Stand
+		default:
+			fmt.Println("Invalid option:", input)
+		}
+	}
+}
+
+func (ai *HumanAI) Summary(hand [][]deck.Card, dealer []deck.Card) {
+	fmt.Println("==FINAL HANDS==")
+	fmt.Println("Player:", hand)
+	fmt.Println("Dealer:", dealer)
+}
