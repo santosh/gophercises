feb83e468f510c4500cec44f6efe61a38bf8eadb
Santosh Kumar <sntshkmr60@gmail.com>
2020-07-27 01:24:36 +0530

    Add and remove comments

--- renamer/main.go
+++ renamer/main.go
@@ -14,21 +14,22 @@ type file struct {
 }
 
 func main() {
-	// fileName := "birthday_001.txt"
-	// // => Birthday - 1 of 4.txt
-	// newName, err := match(fileName, 4)
-	// if err != nil {
-	// fmt.Println("no match")
-	// os.Exit(1)
-	// }
-	// fmt.Println(newName)
+	// 1. Collect all the files to rename.
+	// 2. Iterate over files to generate new name and invoke rename
+	// with system call.
+
 	dir := "sample"
 	var toRename []file
+
+	// 1. Collect all the files to rename.
+	// look over 'dir' recursively
 	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if info.IsDir() {
 			// do not rename a directory
 			return nil
 		}
+		// append all files and their absolute path from the directory to
+		// toRename slice
 		if _, err := match(info.Name()); err == nil {
 			toRename = append(toRename, file{
 				name: info.Name(),
@@ -37,6 +38,9 @@ func main() {
 		}
 		return nil
 	})
+
+	// 2. Iterate over files to generate new name and invoke rename
+	// with system call.
 	for _, orig := range toRename {
 		var n file
 		var err error
@@ -46,16 +50,23 @@ func main() {
 		}
 		n.path = filepath.Join(dir, n.name)
 		fmt.Printf("mv %s => %s\n", orig.path, n.path)
-		err = os.Rename(orig.path, n.path)
-		if err != nil {
-			fmt.Println("Error renaming:", orig.path, err.Error())
-		}
+		// err = os.Rename(orig.path, n.path)
+		// if err != nil {
+		// 	fmt.Println("Error renaming:", orig.path, err.Error())
+		// }
 	}
 }
 
-// match returns new file name, or an error if the filename
-// didn't match our pattern
-func match(fileName string) (string, error) {
+type matchResult struct {
+	base  string
+	index int
+	ext   string
+}
+
+// match takes a fileName and returns a struct of segments which will
+// used to compose a new file name.
+// returns an error if the filename doesn't match our pattern
+func match(fileName string) (*matchResult, error) {
 	// split by '.'; note that there can be more than
 	// two parts at this point. e.g. 'san.tosh.k.jpg'
 	pieces := strings.Split(fileName, ".")
@@ -72,12 +83,12 @@ func match(fileName string) (string, error) {
 	// join back all elements except the last
 	name := strings.Join(pieces[0:len(pieces)-1], "_")
 
-	// get rid of any padding
+	// remove any padding from the last element of
 	number, err := strconv.Atoi(pieces[len(pieces)-1])
 
 	if err != nil {
-		return "", fmt.Errorf("%s didn't match our pattern")
+		return nil, fmt.Errorf("%s didn't match our pattern")
 	}
 
-	return fmt.Sprintf("%s - %d.%s", strings.Title(name), number, ext), nil
+	return &matchResult{strings.Title(name), number, ext}, nil
 }
