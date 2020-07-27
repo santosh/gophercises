b66329173867a1ff2433da89a76177a30b5c3d2a
Santosh Kumar <sntshkmr60@gmail.com>
2020-07-27 16:33:13 +0530

    Replace with regex solution

--- renamer/main.go
+++ renamer/main.go
@@ -5,10 +5,11 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"sort"
-	"strconv"
-	"strings"
+	"regexp"
 )
+
+var re = regexp.MustCompile("^(.*?) ([0-9]{4}) [(]([0-9]+) of ([0-9]+)[)][.](.+?)$")
+var replaceString = "$2 - $1 - $3 of $4.$5"
 
 func main() {
 	var dry bool
@@ -16,11 +17,8 @@ func main() {
 	flag.Parse()
 
 	// 1. Collect all the files to rename.
-	// 2. Iterate over files to generate new name and invoke rename
-	// with system call.
-
 	walkDir := "sample"
-	toRename := make(map[string][]string)
+	var toRename []string
 
 	// 1. Collect all the files to rename.
 	// look over 'walkDir' recursively
@@ -30,75 +28,38 @@ func main() {
 			return nil
 		}
 
-		curDir := filepath.Dir(path)
-		// collect all files and their absolute path from the directory to
-		// toRename slice
-		if m, err := match(info.Name()); err == nil {
-			// example key: sample/picture.jpg
-			// example key: sample/subdir/picture.jpg
-			// example val: [picture_001.txt picture_002.txt picture_003.txt]
-			key := filepath.Join(curDir, fmt.Sprintf("%s.%s", m.base, m.ext))
-			toRename[key] = append(toRename[key], info.Name())
+		if _, err := match(info.Name()); err == nil {
+			toRename = append(toRename, path)
 		}
+
 		return nil
 	})
 
 	// 2. Iterate over files to generate new name and invoke rename
-	// with system call.
-	for key, files := range toRename {
-		dir := filepath.Dir(key)
-		n := len(files)
-		sort.Strings(files)
+	// with a system call.
+	for _, oldPath := range toRename {
+		dir := filepath.Dir(oldPath)
+		filename := filepath.Base(oldPath)
+		newFilename, _ := match(filename)
+		newPath := filepath.Join(dir, newFilename)
 
-		// rename on the basis of index
-		for fileIndex, filename := range files {
-			res, _ := match(filename)
-			newFilename := fmt.Sprintf("%s - %d of %d.%s", res.base, (fileIndex + 1), n, res.ext)
-			oldPath := filepath.Join(dir, filename)
-			newPath := filepath.Join(dir, newFilename)
-			fmt.Printf("mv %s => %s\n", oldPath, newPath)
-			if !dry {
-				err := os.Rename(oldPath, newPath)
-				if err != nil {
-					fmt.Println("Error renaming:", oldPath, newPath, err.Error())
-				}
+		fmt.Printf("mv %s => %s\n", oldPath, newPath)
+		if !dry {
+			err := os.Rename(oldPath, newPath)
+			if err != nil {
+				fmt.Println("Error renaming:", oldPath, newPath, err.Error())
 			}
 		}
 	}
 }
 
-type matchResult struct {
-	base  string
-	index int
-	ext   string
-}
-
-// match takes a fileName and returns a struct of segments which will
-// used to compose a new file name.
-// returns an error if the filename doesn't match our pattern
-func match(fileName string) (*matchResult, error) {
-	// split by '.'; note that there can be more than
-	// two parts at this point. e.g. 'san.tosh.k.jpg'
-	pieces := strings.Split(fileName, ".")
-
-	// take the last element of the slice
-	ext := pieces[len(pieces)-1]
-
-	// join the rest of the elements back
-	fileName = strings.Join(pieces[0:len(pieces)-1], ".")
-
-	// this time split by '_'
-	pieces = strings.Split(fileName, "_")
-
-	// join back all elements except the last
-	name := strings.Join(pieces[0:len(pieces)-1], "_")
-
-	// remove any padding from the last element of
-	number, err := strconv.Atoi(pieces[len(pieces)-1])
-
-	if err != nil {
-		return nil, fmt.Errorf("%s didn't match our pattern", fileName)
+// match takes a fileName and tries to match is with 're' regex,
+// if matched, it replaces/renames it with replaceString
+// throws an error if regex doesn't match.
+func match(fileName string) (string, error) {
+	if !re.MatchString(fileName) {
+		return "", fmt.Errorf("%s didn't match our pattern", fileName)
 	}
 
-	return &matchResult{strings.Title(name), number, ext}, nil
+	return re.ReplaceAllString(fileName, replaceString), nil
 }
