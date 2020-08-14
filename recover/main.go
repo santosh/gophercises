66f0b36d225459a4f1aa3817bf50c4f0d5ee8be9
Santosh Kumar <sntshkmr60@gmail.com>
2020-08-14 08:48:48 +0530

    Add ResponseWriter Wrapper
    The responseWriter wrapper addresses the status code
    and response already been set even if error occurs in
    later response making.

--- recover/main.go
+++ recover/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/panic/", panicDemo)
-	mux.HandleFunc("/panic-after", panicAfterDemo)
+	mux.HandleFunc("/panic-after/", panicAfterDemo)
 	mux.HandleFunc("/", hello)
 	log.Fatal(http.ListenAndServe(":3000", recoverMw(mux, true)))
 }
@@ -30,8 +30,38 @@ func recoverMw(app http.Handler, dev bool) http.HandlerFunc {
 			}
 		}()
 
-		app.ServeHTTP(w, r)
+		nw := &responseWriter{ResponseWriter: w}
+		app.ServeHTTP(nw, r)
+		nw.flush()
 	}
+}
+
+type responseWriter struct {
+	http.ResponseWriter
+	writes [][]byte
+	status int
+}
+
+func (rw *responseWriter) Write(b []byte) (int, error) {
+	rw.writes = append(rw.writes, b)
+	return len(b), nil
+}
+
+func (rw *responseWriter) WriteHeader(statusCode int) {
+	rw.status = statusCode
+}
+
+func (rw *responseWriter) flush() error {
+	if rw.status != 0 {
+		rw.ResponseWriter.WriteHeader(rw.status)
+	}
+	for _, write := range rw.writes {
+		_, err := rw.ResponseWriter.Write(write)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func panicDemo(w http.ResponseWriter, r *http.Request) {
