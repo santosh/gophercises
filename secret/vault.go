f49d302d790eb243c7af9ab12a9c39a87917f6b2
Santosh Kumar <sntshkmr60@gmail.com>
2020-08-28 08:57:55 +0530

    Refactor

--- secret/vault.go
+++ secret/vault.go
@@ -3,13 +3,11 @@ package secret
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
 	"io"
 	"os"
-	"strings"
 	"sync"
 
-	"github.com/santosh/gophercises/secret/encrypt"
+	"github.com/santosh/gophercises/secret/cipher"
 )
 
 // File takes in an encodingKey and filepath. This encryption key
@@ -29,57 +27,43 @@ type Vault struct {
 	keyValues   map[string]string
 }
 
-// loadKeyValues decrypt the encrypted file and fetch the data.
-func (v *Vault) loadKeyValues() error {
+// load decrypt the encrypted file and fetch the data.
+func (v *Vault) load() error {
 	f, err := os.Open(v.filepath)
 	if err != nil {
 		v.keyValues = make(map[string]string)
 		return nil
 	}
 	defer f.Close()
-
-	var sb strings.Builder
-	_, err = io.Copy(&sb, f)
+	r, err := cipher.DecryptReader(v.encodingKey, f)
 	if err != nil {
 		return err
 	}
-	decryptedJSON, err := encrypt.Decrypt(v.encodingKey, sb.String())
-	if err != nil {
-		return err
-	}
-
-	r := strings.NewReader(decryptedJSON)
-	dec := json.NewDecoder(r)
-	err = dec.Decode(&v.keyValues)
-	if err != nil {
-		return err
-	}
-	return nil
+	return v.readKeyValues(r)
 }
 
-// saveKeyValues encrypts the key-value pair with given encodingKey
-func (v *Vault) saveKeyValues() error {
-	var sb strings.Builder
-	enc := json.NewEncoder(&sb)
-	err := enc.Encode(v.keyValues)
-	if err != nil {
-		return err
-	}
-	encryptedJSON, err := encrypt.Encrypt(v.encodingKey, sb.String())
-	if err != nil {
-		return err
-	}
+func (v *Vault) readKeyValues(r io.Reader) error {
+	dec := json.NewDecoder(r)
+	return dec.Decode(&v.keyValues)
+}
+
+// save encrypts the key-value pair with given encodingKey
+func (v *Vault) save() error {
 	f, err := os.OpenFile(v.filepath, os.O_RDWR|os.O_CREATE, 0755)
 	if err != nil {
 		return err
 	}
 	defer f.Close()
-
-	_, err = fmt.Fprint(f, encryptedJSON)
+	w, err := cipher.EncryptWriter(v.encodingKey, f)
 	if err != nil {
 		return err
 	}
-	return nil
+	return v.writeKeyValues(w)
+}
+
+func (v *Vault) writeKeyValues(w io.Writer) error {
+	enc := json.NewEncoder(w)
+	return enc.Encode(&v.keyValues)
 }
 
 // Get is a threadsafe method to Vault to get a key from the Vault
@@ -87,7 +71,7 @@ func (v *Vault) Get(key string) (string, error) {
 	v.mutex.Lock()
 	defer v.mutex.Unlock()
 
-	err := v.loadKeyValues()
+	err := v.load()
 	if err != nil {
 		return "", err
 	}
@@ -104,11 +88,11 @@ func (v *Vault) Set(key, value string) error {
 	v.mutex.Lock()
 	defer v.mutex.Unlock()
 
-	err := v.loadKeyValues()
+	err := v.load()
 	if err != nil {
 		return err
 	}
 	v.keyValues[key] = value
-	err = v.saveKeyValues()
+	err = v.save()
 	return err
 }
