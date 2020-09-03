9ec022e9f6a883c52b79f471f3407f946405d51f
Santosh Kumar <sntshkmr60@gmail.com>
2020-09-04 00:41:25 +0530

    Wrap up Image Transform Service

--- transform/main.go
+++ transform/main.go
@@ -3,25 +3,35 @@ package main
 import (
 	"errors"
 	"fmt"
+	"html/template"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
+	"time"
 
 	"github.com/santosh/gophercises/transform/primitive"
 )
 
 func main() {
+	go func() {
+		t := time.NewTicker(5 * time.Minute)
+		for {
+			<-t.C
+		}
+	}()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		html := `<html><body>
-		<form action="/upload" method="post" enctype="multipart/form-data">
-			<input type="file" name="image">
-			<button type="submit">Upload Image</button>
-		</form>
-		</body></html>`
+			<form action="/upload" method="post" enctype="multipart/form-data">
+				<input type="file" name="image">
+				<button type="submit">Upload Image</button>
+			</form>
+			</body></html>`
 		fmt.Fprint(w, html)
 	})
 	mux.HandleFunc("/modify/", func(w http.ResponseWriter, r *http.Request) {
@@ -31,8 +41,29 @@ func main() {
 			return
 		}
 		defer f.Close()
-		w.Header().Set("Content-Type", "image/png")
-		io.Copy(w, f)
+		ext := filepath.Ext(f.Name())[1:]
+		modeStr := r.FormValue("mode")
+		if modeStr == "" {
+			renderModeChoices(w, r, f, ext)
+			return
+		}
+		mode, err := strconv.Atoi(modeStr)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		nStr := r.FormValue("n")
+		if nStr == "" {
+			renderNumShapeChoices(w, r, f, ext, primitive.Mode(mode))
+			return
+		}
+		numShapes, err := strconv.Atoi(nStr)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		_ = numShapes
+		http.Redirect(w, r, "/img/"+filepath.Base(f.Name()), http.StatusFound)
 	})
 	mux.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
 		file, header, err := r.FormFile("image")
@@ -45,30 +76,122 @@ func main() {
 		onDisk, err := tempfile("", ext)
 		if err != nil {
 			http.Error(w, "Something went wrong", http.StatusInternalServerError)
+			return
 		}
-		_, err = io.Copy(onDisk, file)
 		defer onDisk.Close()
+		_, err = io.Copy(onDisk, file)
 		if err != nil {
 			http.Error(w, "Something went wrong", http.StatusInternalServerError)
+			return
 		}
 		http.Redirect(w, r, "/modify/"+filepath.Base(onDisk.Name()), http.StatusFound)
 	})
+
 	fs := http.FileServer(http.Dir("./img/"))
 	mux.Handle("/img/", http.StripPrefix("/img", fs))
 	log.Fatal(http.ListenAndServe(":3000", mux))
 }
 
-func tempfile(prefix, ext string) (*os.File, error) {
-	in, err := ioutil.TempFile("./img", prefix)
-	if err != nil {
-		return nil, errors.New("main: failed to create temporary input file")
+func renderNumShapeChoices(w http.ResponseWriter, r *http.Request, rs io.ReadSeeker, ext string, mode primitive.Mode) {
+	opts := []genOpts{
+		{N: 10, M: mode},
+		{N: 20, M: mode},
+		{N: 30, M: mode},
+		{N: 40, M: mode},
 	}
-	defer os.Remove(in.Name())
-	return os.Create(fmt.Sprintf("%s.%s", in.Name(), ext))
+	imgs, err := genImages(rs, ext, opts...)
+	if err != nil {
+		panic(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	html := `<html><body>
+			{{range .}}
+				<a href="/modify/{{.Name}}?mode={{.Mode}}&n={{.NumShapes}}">
+					<img style="width: 20%;" src="/img/{{.Name}}">
+				</a>
+			{{end}}
+			</body></html>`
+	tpl := template.Must(template.New("").Parse(html))
+	type dataStruct struct {
+		Name      string
+		Mode      primitive.Mode
+		NumShapes int
+	}
+	var data []dataStruct
+	for i, img := range imgs {
+		data = append(data, dataStruct{
+			Name:      filepath.Base(img),
+			Mode:      opts[i].M,
+			NumShapes: opts[i].N,
+		})
+	}
+	err = tpl.Execute(w, data)
+	if err != nil {
+		panic(err)
+	}
 }
 
-func genImage(file io.Reader, ext string, numShapes int, mode primitive.Mode) (string, error) {
-	out, err := primitive.Transform(file, ext, 50, primitive.WithMode(mode))
+func renderModeChoices(w http.ResponseWriter, r *http.Request, rs io.ReadSeeker, ext string) {
+	opts := []genOpts{
+		{N: 10, M: primitive.ModeCircle},
+		{N: 10, M: primitive.ModeBeziers},
+		{N: 10, M: primitive.ModePolygon},
+		{N: 10, M: primitive.ModeCombo},
+	}
+	imgs, err := genImages(rs, ext, opts...)
+	if err != nil {
+		panic(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	html := `<html><body>
+			{{range .}}
+				<a href="/modify/{{.Name}}?mode={{.Mode}}">
+					<img style="width: 20%;" src="/img/{{.Name}}">
+				</a>
+			{{end}}
+			</body></html>`
+	tpl := template.Must(template.New("").Parse(html))
+	type dataStruct struct {
+		Name string
+		Mode primitive.Mode
+	}
+	var data []dataStruct
+	for i, img := range imgs {
+		data = append(data, dataStruct{
+			Name: filepath.Base(img),
+			Mode: opts[i].M,
+		})
+	}
+	err = tpl.Execute(w, data)
+	if err != nil {
+		panic(err)
+	}
+}
+
+type genOpts struct {
+	N int
+	M primitive.Mode
+}
+
+func genImages(rs io.ReadSeeker, ext string, opts ...genOpts) ([]string, error) {
+	var ret []string
+	for _, opt := range opts {
+		rs.Seek(0, 0)
+		f, err := genImage(rs, ext, opt.N, opt.M)
+		if err != nil {
+			return nil, err
+		}
+		ret = append(ret, f)
+	}
+	return ret, nil
+}
+
+func genImage(r io.Reader, ext string, numShapes int, mode primitive.Mode) (string, error) {
+	out, err := primitive.Transform(r, ext, numShapes, primitive.WithMode(mode))
 	if err != nil {
 		return "", err
 	}
@@ -79,4 +202,13 @@ func genImage(file io.Reader, ext string, numShapes int, mode primitive.Mode) (s
 	defer outFile.Close()
 	io.Copy(outFile, out)
 	return outFile.Name(), nil
+}
+
+func tempfile(prefix, ext string) (*os.File, error) {
+	in, err := ioutil.TempFile("./img/", prefix)
+	if err != nil {
+		return nil, errors.New("main: failed to create temporary file")
+	}
+	defer os.Remove(in.Name())
+	return os.Create(fmt.Sprintf("%s.%s", in.Name(), ext))
 }
--- transform/primitive/primitive.go
+++ transform/primitive/primitive.go
@@ -6,30 +6,29 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"os"
 	"os/exec"
 	"strings"
 )
 
-// Mode defies the shapes used when transforming images.
+// Mode defines the shapes used when transforming images.
 type Mode int
 
-// Modes supported by tne primivite package.
-// 0=combo, 1=triangle, 2=rect, 3=ellipse, 4=circle, 5=rotatedrect, 6=beziers, 7=rotatedellipse, 8=polygon
+// Modes supported by the primitive package.
 const (
 	ModeCombo Mode = iota
 	ModeTriangle
 	ModeRect
 	ModeEllipse
 	ModeCircle
-	ModeRotatedrect
+	ModeRotatedRect
 	ModeBeziers
-	ModeRotatedellipse
+	ModeRotatedEllipse
 	ModePolygon
 )
 
-// WithMode is an option for the Transform function that will define the mode you want to use. By default, ModeTriangle will be used.
+// WithMode is an option for the Transform function that will define the
+// mode you want to use. By default, ModeTriangle will be used.
 func WithMode(mode Mode) func() []string {
 	return func() []string {
 		return []string{"-m", fmt.Sprintf("%d", mode)}
@@ -37,9 +36,8 @@ func WithMode(mode Mode) func() []string {
 }
 
 // Transform will take the provided image and apply a primitive
-// transform to it, then return a reader to the resulting image.
-func Transform(image io.Reader, ext string, numShapes int,
-	opts ...func() []string) (io.Reader, error) {
+// transformation to it, then return a reader to the resulting image.
+func Transform(image io.Reader, ext string, numShapes int, opts ...func() []string) (io.Reader, error) {
 	var args []string
 	for _, opt := range opts {
 		args = append(args, opt()...)
@@ -47,33 +45,35 @@ func Transform(image io.Reader, ext string, numShapes int,
 
 	in, err := tempfile("in_", ext)
 	if err != nil {
-		log.Fatalf("%s", err)
 		return nil, errors.New("primitive: failed to create temporary input file")
 	}
 	defer os.Remove(in.Name())
-
 	out, err := tempfile("in_", ext)
 	if err != nil {
 		return nil, errors.New("primitive: failed to create temporary output file")
 	}
 	defer os.Remove(out.Name())
 
-	// 1. Read image into in file
+	// Read image into in file
 	_, err = io.Copy(in, image)
 	if err != nil {
-		return nil, errors.New("primitive: failed to copy image into temporary input file")
+		return nil, errors.New("primitive: failed to copy image into temp input file")
 	}
-	// 2. Run primitive w/ -i in.Name() -o out.Name()
+
+	// Run primitive w/ -i in.Name() -o out.Name()
 	stdCombo, err := primitive(in.Name(), out.Name(), numShapes, args...)
 	if err != nil {
 		return nil, fmt.Errorf("primitive: failed to run the primitive command. stdcombo=%s", stdCombo)
 	}
+	// if strings.TrimSpace(stdCombo) == "" {
+	// 	panic(stdCombo)
+	// }
 
-	// 3. Read out into a reader, return reader, delete out
+	// read out into a reader, return reader, delete out
 	b := bytes.NewBuffer(nil)
 	_, err = io.Copy(b, out)
 	if err != nil {
-		return nil, errors.New("primitive: failed to copy output file into byte buffer")
+		return nil, errors.New("primitive: Failed to copy output file into byte buffer")
 	}
 	return b, nil
 }
@@ -89,7 +89,7 @@ func primitive(inputFile, outputFile string, numShapes int, args ...string) (str
 func tempfile(prefix, ext string) (*os.File, error) {
 	in, err := ioutil.TempFile("", prefix)
 	if err != nil {
-		return nil, errors.New("primitive: failed to create temporary input file")
+		return nil, errors.New("primitive: failed to create temporary file")
 	}
 	defer os.Remove(in.Name())
 	return os.Create(fmt.Sprintf("%s.%s", in.Name(), ext))
