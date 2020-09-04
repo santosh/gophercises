107f5519b36b194565e4302ce639655da6615e13
Santosh Kumar <sntshkmr60@gmail.com>
2020-09-04 19:55:04 +0530

    Add Image

--- pdf/main.go
+++ pdf/main.go
@@ -37,8 +37,31 @@ func main() {
 		{60, 300},
 	}, "F")
 
+	pdf.ImageOptions("images/jump.png", 275, 275, 92, 0, false, gofpdf.ImageOptions{
+		ReadDpi: true,
+	}, 0, "")
+
+	// Grid
+	drawGrid(pdf)
+
 	err := pdf.OutputFileAndClose("p1.pdf")
 	if err != nil {
 		panic(err)
+	}
+}
+
+func drawGrid(pdf *gofpdf.Fpdf) {
+	w, h := pdf.GetPageSize()
+	pdf.SetFont("courier", "", 12)
+	pdf.SetTextColor(80, 80, 80)
+	pdf.SetDrawColor(200, 200, 200)
+	for x := 0.0; x < w; x = x + (w / 20.0) {
+		pdf.Line(x, 0, x, h)
+		_, lineHt := pdf.GetFontSize()
+		pdf.Text(x, lineHt, fmt.Sprintf("%d", int(x)))
+	}
+	for y := 0.0; y < h; y = y + (h / 20.0) {
+		pdf.Line(0, y, w, y)
+		pdf.Text(0, y, fmt.Sprintf("%d", int(y)))
 	}
 }
