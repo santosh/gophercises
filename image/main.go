92a5bd2af70900bcbb24cefe6c7dd6f983837acb
Santosh Kumar <sntshkmr60@gmail.com>
2020-09-04 15:39:45 +0530

    Implement bar graph in svgo

--- image/main.go
+++ image/main.go
@@ -1,61 +1,22 @@
 package main
 
 import (
-	"image"
-	"image/color"
-	"image/draw"
-	"image/png"
+	"math/rand"
 	"os"
+
+	svg "github.com/ajstarks/svgo"
 )
 
+func rn(n int) int { return rand.Intn(n) }
+
 func main() {
+	canvas := svg.New(os.Stdout)
 	data := []int{100, 33, 73, 64}
-
-	w, h := len(data)*60+10, 100
-	r := image.Rect(0, 0, w, h)
-	img := image.NewRGBA(r)
-	bg := image.NewUniform(color.RGBA{240, 240, 240, 255})
-
-	draw.Draw(img, r, bg, image.Point{0, 0}, draw.Src)
-
-	mask := image.NewRGBA(image.Rect(0, 0, w, h))
-	for y := 0; y < h; y++ {
-		for x := 0; x < w; x++ {
-			alpha := uint8(0)
-			switch {
-			case y < 30:
-				alpha = 255
-			case y < 50:
-				alpha = 100
-			}
-			mask.Set(x, y, color.RGBA{
-				R: uint8((x + y) & 255),
-				G: uint8((x + y) << 1 & 255),
-				B: uint8((x + y) << 2 & 255),
-				A: alpha,
-			})
-		}
+	width := len(data)*60 + 10
+	height := 100
+	canvas.Start(width, height)
+	for i, val := range data {
+		canvas.Rect(i*60+10, height-val, 50, val, "fill:rgb(77, 200, 232)")
 	}
-
-	for i, dp := range data {
-		x0, y0 := (i*60 + 10), 100-dp
-		x1, y1 := (i+1)*60-1, 100
-		bar := image.Rect(x0, y0, x1, y1)
-		grey := image.NewUniform(color.RGBA{180, 180, 180, 255})
-		draw.Draw(img, bar, grey, image.Point{0, 0}, draw.Src)
-
-		red := image.NewUniform(color.RGBA{255, 0, 0, 255})
-		draw.DrawMask(img, bar, red, image.Point{0, 0}, mask, image.Point{x0, y0}, draw.Over)
-
-	}
-
-	f, err := os.Create("image.png")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-	err = png.Encode(f, img)
-	if err != nil {
-		panic(err)
-	}
+	canvas.End()
 }
