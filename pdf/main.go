b359ef546344f923211d5a3c503028b70539b69f
Santosh Kumar <sntshkmr60@gmail.com>
2020-09-04 19:22:55 +0530

    Playing with text
    - SetTextColor
    - SetFont
    - MultiCell

--- pdf/main.go
+++ pdf/main.go
@@ -13,9 +13,18 @@ func main() {
 	pdf.AddPage()
 
 	// Basic Text Stuff
+	pdf.MoveTo(0, 0)
 	pdf.SetFont("arial", "B", 16)
 	_, lineHt := pdf.GetFontSize()
+	pdf.SetTextColor(255, 0, 0)
 	pdf.Text(0, lineHt, "Hello, world!")
+	pdf.MoveTo(0, lineHt*2.0)
+
+	pdf.SetFont("times", "", 18)
+	pdf.SetTextColor(100, 100, 100)
+	_, lineHt = pdf.GetFontSize()
+	pdf.MultiCell(0, lineHt*1.5, "Here is some text. If it is too long it will be word wrapped automatically. If there is a new line if will be\nwrapped as well (unlike other ways of writing text in gopdf).", gofpdf.BorderNone, gofpdf.AlignRight, false)
+
 	err := pdf.OutputFileAndClose("p1.pdf")
 	if err != nil {
 		panic(err)
