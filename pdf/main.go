1e1b2671f22d9d691ec76085413687019a128e4c
Santosh Kumar <sntshkmr60@gmail.com>
2020-09-04 19:33:06 +0530

    Create Polygon

--- pdf/main.go
+++ pdf/main.go
@@ -25,6 +25,18 @@ func main() {
 	_, lineHt = pdf.GetFontSize()
 	pdf.MultiCell(0, lineHt*1.5, "Here is some text. If it is too long it will be word wrapped automatically. If there is a new line if will be\nwrapped as well (unlike other ways of writing text in gopdf).", gofpdf.BorderNone, gofpdf.AlignRight, false)
 
+	// Basic Shapes
+	pdf.SetFillColor(0, 255, 0)
+	pdf.SetDrawColor(0, 0, 255)
+	pdf.Rect(10, 100, 100, 100, "FD")
+	pdf.SetFillColor(100, 200, 200)
+	pdf.Polygon([]gofpdf.PointType{
+		{110, 250},
+		{160, 300},
+		{110, 350},
+		{60, 300},
+	}, "F")
+
 	err := pdf.OutputFileAndClose("p1.pdf")
 	if err != nil {
 		panic(err)
