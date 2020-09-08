305203ebb8e5c3f95037ae974b5e0a2c340615a5
Santosh Kumar <sntshkmr60@gmail.com>
2020-09-08 16:07:49 +0530

    Add text to header

--- pdf/main.go
+++ pdf/main.go
@@ -6,17 +6,18 @@ import (
 	"github.com/phpdave11/gofpdf"
 )
 
+const (
+	bannerHt = 95.0
+	xIndent  = 40.0
+)
+
 func main() {
 	pdf := gofpdf.New(gofpdf.OrientationPortrait, gofpdf.UnitPoint, gofpdf.PageSizeLetter, "")
 	w, h := pdf.GetPageSize()
 	fmt.Printf("width=%v, height=%v\n", w, h)
 	pdf.AddPage()
 
-	const (
-		bannerHt = 95.0
-		xIndent  = 40.0
-	)
-
+	// Header
 	pdf.SetFillColor(103, 60, 79)
 	pdf.Polygon([]gofpdf.PointType{
 		{0, 0},
@@ -25,12 +26,33 @@ func main() {
 		{0, bannerHt * 0.9},
 	}, "F")
 
+	// Footer
 	pdf.Polygon([]gofpdf.PointType{
 		{0, h},
 		{0, h - (bannerHt * 0.2)},
 		{w, h - (bannerHt * 0.1)},
 		{0, h},
 	}, "F")
+
+	// Banner - INVOICE
+	pdf.SetFont("arial", "B", 40)
+	pdf.SetTextColor(255, 255, 255)
+	_, lineHt := pdf.GetFontSize()
+	pdf.Text(xIndent, bannerHt-(bannerHt/2.0)+lineHt/3.1, "INVOICE")
+
+	// Header Text
+	pdf.SetFont("arial", "", 12)
+	pdf.SetTextColor(255, 255, 255)
+	_, lineHt = pdf.GetFontSize()
+	pdf.MoveTo(w-xIndent-2.0*124.0, (bannerHt-(lineHt*1.5*3.0))/2.0)
+	pdf.MultiCell(124.0, lineHt*1.5, "(123) 456-7890\njon@calhoun.io\nGophercises.com", gofpdf.BorderNone, gofpdf.AlignRight, false)
+
+	// Banner - Address
+	pdf.SetFont("arial", "", 12)
+	pdf.SetTextColor(255, 255, 255)
+	_, lineHt = pdf.GetFontSize()
+	pdf.MoveTo(w-xIndent-124.0, (bannerHt-(lineHt*1.5*3.0))/2.0)
+	pdf.MultiCell(124.0, lineHt*1.5, "123 Fake St\nSome Town, PA\n12345", gofpdf.BorderNone, gofpdf.AlignRight, false)
 
 	drawGrid(pdf)
 
@@ -46,11 +68,17 @@ func drawGrid(pdf *gofpdf.Fpdf) {
 	pdf.SetTextColor(80, 80, 80)
 	pdf.SetDrawColor(200, 200, 200)
 	for x := 0.0; x < w; x = x + (w / 20.0) {
+		pdf.SetTextColor(200, 200, 200)
 		pdf.Line(x, 0, x, h)
 		_, lineHt := pdf.GetFontSize()
 		pdf.Text(x, lineHt, fmt.Sprintf("%d", int(x)))
 	}
-	for y := 0.0; y < h; y = y + (h / 20.0) {
+	for y := 0.0; y < h; y = y + (w / 20.0) {
+		if y < bannerHt*.9 {
+			pdf.SetTextColor(200, 200, 200)
+		} else {
+			pdf.SetTextColor(80, 80, 80)
+		}
 		pdf.Line(0, y, w, y)
 		pdf.Text(0, y, fmt.Sprintf("%d", int(y)))
 	}
